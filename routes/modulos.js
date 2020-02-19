a59d7093d5226ba6a664be65ce92cc95ff89403c
iccddg <iccdamiandg@gmail.com>
2020-02-18 22:13:20 -0700

    Agregada carga masiva de modulos

--- routes/modulos.js
+++ routes/modulos.js
@@ -5,6 +5,10 @@ var router = express.Router();
 var modulos = require('../api/modulos/gest_modulos')
 var semestres = require('../api/semestres/gest_semestres')
 var carreras = require('../api/carreras/gest_carreras')
+const fs = require('fs');
+const csv = require('csv-parser');
+
+var upload = multer({ dest: 'tempFile/' })
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -41,6 +45,113 @@ router.post('/gest_modulos',upload.none(), function(req, res, next) {
       console.error(err);
       res.status(500).send(err);
   })
+});
+//Ruta para carga masiva de modulos
+router.post('/gest_modulos/cargamasiva',upload.single('modulos'), function(req, res, next) {
+  console.log('Entrada a la ruta /gest_modulos/cargamasiva.post');
+  let p =[];
+  let valores = [];
+  var archivo = req.file.filename;
+  let falla="";
+  // con la funcion carga obtenemos los datos del archivo materias
+  let carga =  new Promise ((reso,reje) => {
+    fs.createReadStream('tempFile/'+ archivo) // Abrir archivo 
+    .pipe(csv()) 
+    .on('data', function(data){ 
+      try {
+        p.push(data); 
+        //perform the operation 
+      } catch(err) { 
+        reje(err);
+        //error handler 
+      } 
+    }) 
+    .on('end', function(){ 
+      //some final operation
+      reso(p);
+    });
+  })
+  //guardamos en sem la lista de semestres con su llave primaria
+  let sem=semestres.lis_semestres();
+  let carr=carreras.lis_carreras();
+  // resolvemos las tres promesas
+  Promise.all([carga,sem,carr])
+    .then(resultados=>{
+      console.log(resultados);
+      let modu=resultados[0];
+      console.log(modu);
+      let seme=resultados[1].r;
+      let carre=resultados[2].r;
+      compara(modu,seme);
+      if(falla !== ""){
+        console.log("se termino");
+      }else{
+        compara2(modu,carre);
+        if(falla !== ""){
+          console.log("se termino");
+        }else{
+          valores.forEach((value,key,valores)=>{
+            console.log(value);
+            modulos.new_modulo(value).then(
+              resolve => {
+                console.log(resolve);
+                if(Object.is(valores.length - 1, key)){
+                  console.log('es el utlimo'+value);
+                  res.status(200);
+                  res.redirect('/modulos/gest_modulos')
+                }
+            }).catch(err => {
+              console.error(err);
+              res.status(500).send(err);
+            })
+           })
+        }
+     };
+    }).catch(err => {
+              console.error(err);
+              res.status(500).send(err);
+    });
+    //compara los dos arreglos y si encuentra en mate un 
+    //semestre que exista en seme, le asigna el pk de ese
+    //semestre en un campo fksemestre
+    function compara(modu,seme){
+      modu.forEach((value,index,modu)=>{
+        let pksemestre = seme.findIndex((val,ind,seme)=>{
+          return val.semestre == value.semestre;
+        })
+        if (pksemestre == -1){
+          falla="ocurrio un error en la linea "+(index+2) +" El semestre no existe en la base de datos";
+          console.error(falla);
+          res.status(500).send(falla);
+        }else{
+          valores.push({modulo:value.modulo,fksemestre:seme[pksemestre].pksemestre});
+          console.log(valores);
+        };
+      return;
+      })
+      return;
+    };
+    //compara los dos arreglos y si encuentra en mate una 
+    //carrera que exista en seme, le asigna el pk de ese
+    //semestre en un campo fksemestre
+    function compara2(modu,carre){
+      console.log(modu);
+      console.log(valores);
+      modu.forEach((value,index,modu)=>{
+        let pkcarrera = carre.findIndex((val,ind,carre)=>{
+          return val.carrera === value.carrera;
+        })
+        if (pkcarrera == -1){
+          falla="ocurrio un error en la linea "+(index+2) +" la carrera no existe en la base de datos";
+          console.error(falla);
+          res.status(500).send(falla);
+        }else{
+          valores[index]={modulo:valores[index].modulo,fksemestre:valores[index].fksemestre,fkcarrera:carre[pkcarrera].pkcarrera};
+        };
+      return;
+      })
+      return;
+    };
 });
 //middleware para todas las direcciones con id/edit, actualmente sin uso
 router.param('id*', async (req, res,next) => {
